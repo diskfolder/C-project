cab2dc4224fcc5f8052d09de3965dd8795eb0bd6
mly <1552998626@qq.com>
2022-10-18 20:11:43 +0800

    10月18日

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -34,27 +34,60 @@
 //
 //	return 0;
 //}
+//int main()
+//{
+//	int rs = 0;
+//	int ret = 0;
+//	char password[20] = { 0 };
+//	printf("请输入密码:>\n");
+//	scanf("%s", password);
+//	while ((rs = getchar()) != '\n');
+//	{
+//		;
+//	}
+//	printf("请确认输入(Y/N)");
+//	ret = getchar();
+//	if (ret == 'Y')
+//	{
+//
+//		printf("确认成功");
+//
+//	}
+//	else
+//	{
+//
+//		printf("放弃确认");
+//	}
+//
+//
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//for (a = 1; a < 11; a++)
+//	{
+//		if (a == 5)
+//			continue;
+//		printf("%d ", a);
+//	}
+//
+//	return 0;
+//}
 int main()
 {
-	int ret = 0;
-	char password[20] = { 0 };
-	printf("请输入密码:>");
-	scanf("%s", password);
-	printf("请确认输入(Y/N)");
-	ret = getchar();
-	if (ret == 'Y')
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	for (a=0; a < 10; a++,c++)
 	{
-
-		printf("确认成功");
-
+		for (b=0; b < 10; b++,c++)
+		{
+			printf("hehe %d\n", c);
+		}
 	}
-	else
-	{
-
-		printf("放弃确认");
-	}
-
-
 
 
 	return 0;
---
+++ 分支与循环/分支与循环/源.cpp
@@ -0,0 +1,53 @@
+#define  _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	scanf("%d", &a);
+//	if (a > 0 && a < 10)
+//		printf("小孩子");
+//	else if (a >= 10 && a < 20)
+//		printf("青少年");
+//	else if (a >= 20 && a < 30)
+//		printf("壮年");
+//	else if (a >= 30 && a < 40)
+//		printf("中年人");
+//	else if (a >= 40)
+//		printf("你老了");
+//
+//
+//
+//
+//	return 0;
+//}
+//int main()
+//{
+//	int day = 0;
+//	scanf("%d", &day);
+//	switch (day)
+//	{
+//	case 1:
+//		printf("星期一\n");
+//		break;
+//	case 2:
+//		printf("星期二\n");
+//		break;
+//	case 3:
+//		printf("星期三\n");
+//		break;
+//	case 4:
+//		printf("星期四\n");
+//		break;
+//	case 5:
+//		printf("星期五\n");
+//		break;
+//	case 6:
+//		printf("星期六\n");
+//		break;
+//	case 7:
+//		printf("星期天\n");
+//		break;//switch可以嵌套
+//	}
+//
+//
+//	return 0;
